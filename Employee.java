46e6627eb68d28ca7a045e9eceb0d1b7561bbae7
UnkalRevathi <unkalrevathi@gmail.com>
2025-05-06 14:44:45 +0530

    saved

--- Employee.java
+++ Employee.java
@@ -2,18 +2,21 @@ public class Employee {
     String name;
     int id;
     double salary;
-    public Employee(String name,int id, double salary){
+    String Dependencies;
+    public Employee(String name,int id, double salary,String Dependencies){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.Dependencies=Dependencies;
     }
     public void displayInfo(){
         System.out.println("Employee ID:"+id);
         System.out.println("Employee Name:"+name);
         System.out.println("Salary:"+salary);
+        System.out.println("Dependencies: "+Dependencies);
     }
     public static void main(String[] args){
-        Employee emp1=new Employee("XYZ", 101, 50000.0);
+        Employee emp1=new Employee("XYZ", 101, 50000.0,"Brother");
         emp1.displayInfo();
     }
     
