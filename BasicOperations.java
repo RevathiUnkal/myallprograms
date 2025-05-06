f602af54990ef361bf6ca68e4598af5089736323
UnkalRevathi <unkalrevathi@gmail.com>
2025-05-06 15:33:28 +0530

    saved

---
+++ BasicOperations.java
@@ -0,0 +1,12 @@
+public class BasicOperations {
+public static void main(String []args){
+    int a=12;
+    int b=4;
+    int sum=a+b;
+    int diff=a-b;
+    int prod=a*b;
+    System.out.println("Additon is "+sum);
+    System.out.println("Subtraction is "+diff);
+    System.out.println("Product is "+prod);
+}
+}
