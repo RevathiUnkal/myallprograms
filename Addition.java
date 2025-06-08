b89a8fc9ef96265fc56cd2ec8d965adb2cfd57c1
RevathiUnkal <unkalrevathi@gmail.com>
2025-06-08 18:49:04 +0530

    saved

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+        
+    
+    int a=23;
+    int b=24;
+    int c=a+b;
+    System.out.println("Addition result is"+c);
+       }
+
+}
